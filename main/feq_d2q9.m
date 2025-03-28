4547d2a8e5835123ccbf7804deaba9e2b8159fbd
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2025-03-28 10:33:00 -0400

    Fixed feq function

--- main/feq_d2q9.m
+++ main/feq_d2q9.m
@@ -24,7 +24,7 @@ function [feq] = feq_d2q9(rho, u, E, row, col)
     
     b = pagemtimes(pagetranspose(E), u);
 
-    guh = (1 + (b)/(cs^2) + ((b).^2)/(cs^4) - sum(u.^2, 1)/(2*(cs^2)));
+    guh = (1 + (b)./(cs^2) + ((b).^2)./(2*(cs^4)) - sum(u.^2, 1)./(2*(cs^2)));
 
     feq = w.*guh.*rho;
 end
--- main/rhoNu.m
+++ main/rhoNu.m
@@ -3,6 +3,5 @@ function [rho, u] = rhoNu(f, E)
     rho = sum (f, 1);
 
     u = pagemtimes(E, f)./rho;
-    
 end
 
